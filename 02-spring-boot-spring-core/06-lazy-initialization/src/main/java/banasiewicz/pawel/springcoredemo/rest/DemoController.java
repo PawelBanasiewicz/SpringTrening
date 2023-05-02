30be21e7b4d9344104f55a7af9868cf3118bb3d5
Paweł Banasiewicz <pawel.j.banasiewicz@gmail.com>
2023-05-02 20:27:45 +0200

    add lazy annotation

---
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/banasiewicz/pawel/springcoredemo/rest/DemoController.java
@@ -0,0 +1,25 @@
+package banasiewicz.pawel.springcoredemo.rest;
+
+import banasiewicz.pawel.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    @Autowired
+    public DemoController(@Qualifier("cricketCoach") Coach coach) {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+        myCoach = coach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkOut();
+    }
+}
