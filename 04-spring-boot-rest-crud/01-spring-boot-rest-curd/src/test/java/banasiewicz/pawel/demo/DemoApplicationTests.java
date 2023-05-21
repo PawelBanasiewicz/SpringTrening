baffce163358015e85cad87490f8f22ef3bd3e38
Paweł Banasiewicz <pawel.j.banasiewicz@gmail.com>
2023-05-21 13:50:04 +0200

    add restController

---
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-curd/src/test/java/banasiewicz/pawel/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package banasiewicz.pawel.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
